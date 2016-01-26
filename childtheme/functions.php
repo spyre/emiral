2f49713a5294b5dab94d7c049c20c7e4e0973950
spyre <spirescualex@gmail.com>
2016-01-26 16:51:53 +0200

    child theme, JSON, REST ws, WordPress web services

---
+++ childtheme/functions.php
@@ -0,0 +1,11 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+}
+
+function childThemeTestFunction(){
+	echo 'This is specific to the child theme<br/>';
+}
+?>
